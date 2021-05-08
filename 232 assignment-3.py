00ea6cc78728d3645725dbbb958cf2abe6f85064
Gülser Öykü Yılmaz <80680708+gulseroykumetu@users.noreply.github.com>
2021-05-08 14:56:30 +0300

    Add files via upload

---
+++ 232 assignment-3.py
@@ -0,0 +1,13 @@
+abc = 'Ey Doğu Antepte Kebap Ye'
+
+a = abc.casefold()
+b = a.replace(" ", "")
+
+print(b[::-1])
+
+abc1 = 'What are you doing ?'
+
+x = abc1.casefold()
+y =x.replace(" ", "")
+
+print(y[::-1])
